d85b48f8dcd99e7e87ae4c70e4d6d0ec607feb46
Mariana Faustino <marifaustino@gmail.com>
2023-03-27 18:26:29 -0300

    useCallback

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,6 +6,8 @@ import HookUseEffect from "../components/HookUseEffect"
 import HookUseReducer from "../components/HookUseReducer"
 import HookUseState from "../components/HookUseState"
 import HookUseRef from "../components/HookUseRef"
+import HookUseCallback from "../components/HookUseCallback"
+import HookUseMemo from "../components/HookUseMemo"
 
 const Home = () => {
 
@@ -21,6 +23,8 @@ const Home = () => {
         <p>Valor do contexto: {contextValue}</p>
         <hr />
         <HookUseRef/>
+        <HookUseCallback/>
+        <HookUseMemo/>
     </div>
   )
 }
